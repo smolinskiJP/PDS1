6819f653374694a5524f69d5f38c780f36655767
smolinskiJP <104567183+smolinskiJP@users.noreply.github.com>
2024-03-19 13:10:06 -0300

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define SUCESSO 0
+#define TAM 10
+
+int main(int argc, char **argv)
+{
+	int vetor[TAM];
+	for(int i = 0; i < TAM; i++){
+		scanf(" %d", &vetor[i]);
+	}
+	for(int i = TAM-1; i >=0; i--){
+		printf("%d\n", vetor[i]);
+	}
+	return SUCESSO;
+}
