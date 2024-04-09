da8d8451b50d7ff497132fb60e8d13a2e156737f
smolinskiJP <104567183+smolinskiJP@users.noreply.github.com>
2024-04-09 12:44:05 -0300

    Update 43.c

--- 43.c
+++ 43.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #define SUCESSO 0
-#define TAM 10
+#define START 233
+#define END 457
 
-int main(int argc, char **argv)
-{
-	int vetorE[TAM], vetorS[TAM];
-	for(int i = 0; i < TAM; i++){
-		scanf(" %d", &vetorE[i]);
-		vetorS[TAM-(i+1)] = vetorE[i];
-	}
-	printf("\n");
-	for(int i = 0; i < TAM; i++){
-		printf("%d\n", vetorS[i]);
-	}
+int main(int argc, char** argv){
+	
+	int i = START;
+	
+	do{
+		printf("%d\n", i);
+		if(i > 300 && i < 400) i += 3;
+		else i += 5;
+	}while(i <= 457);
+	
 	return SUCESSO;
 }
